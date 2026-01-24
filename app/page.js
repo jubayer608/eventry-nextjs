8dfcd6088cb04203637036c7fbe9b17b12d611d3
Jubayer Hasan <jubaihasan88@gmail.com>
2026-01-25 00:13:34 +0600

    completed all components structure

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
      <section class="container"> 
        <Header></Header>
+       <EventList></EventList>
      </section>
   );
 }
